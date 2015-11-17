1c58ace3b5cf677600cbf5bdbe727b444a39500d
Christian Folini <email_github.com@tikon.ch>
2015-11-17 14:20:14 +0100

    labot-07: added graph, its generation and raw logfiles

---
+++ labor-07/gnuplot-graph/generate-graph.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+N=$(dpkg -l | grep ttf-bitstream-vera | wc -l)
+
+if [ $N -lt 1 ]; then
+	echo "Please install ubuntu package ttf-bitstream-vera. It is not installed."
+	exit 1
+fi
+
+export GDFONTPATH=/usr/share/fonts/truetype/ttf-bitstream-vera
+
+gnuplot ./gnuplot-xkcd-raw-linscale.gp
